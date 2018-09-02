00416df8e692650ed53bcfdd2f719d83130c5577
Michael Hübner <m.huebner@email.com>
2018-09-02 15:33:48 +0200

    Landingpage v. 1.0, 02.09.2018

--- app.js
+++ app.js
@@ -1,20 +1,102 @@
-var express         = require ("express"),
+var cookieParser    = require('cookie-parser'),
+    nodemailer      = require('nodemailer'),
+    bodyParser      = require ("body-parser"),
+    express         = require ("express"),
+    session         = require('express-session'),
+    flash           = require ("connect-flash"),
     app             = express();
 
 
 
 /* +++++++++++++++++++++++++++ APP CONFIGURATION +++++++++++++++++++++++++++ */
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+require('dotenv').config();
 app.set("view engine", "ejs");
-
 app.use("/public", express.static(__dirname + "/public"));
 app.use("/views", express.static(__dirname + "/views"));
-app.use("/semantic", express.static(__dirname + "/semantic"));
+app.use("/node_modules/cookieconsent", express.static(__dirname + "/node_modules/cookieconsent"));
+app.use(cookieParser());
+app.use(require("express-session")({
+    secret: process.env.SESSIONSECRET,
+    cookie: {_expires : 30000000},
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(flash());
 
+/* +++++++++++++++++++++++++ GLOBAL ELEMENTS CONFIGURATION +++++++++++++++++++++++++ */
+app.use(function(req, res, next){
+    res.locals.error = req.flash("error");
+    res.locals.success = req.flash("success");
+    next();
+ });
 
 /* ++++++++++++++++++++++++++++++ APP ROUTING ++++++++++++++++++++++++++++++ */
 /* ROOT ROUTE/ LANDING */
 app.get("/", function(req, res){
     res.render("landing");
+});
+
+/* IMPRESSUM ROUTE */
+app.get("/impressum", function(req, res){
+    res.render("impressum");
+});
+
+/* DATENSCHUTZ ROUTE */
+app.get("/datenschutz", function(req, res){
+    res.render("datenschutz");
+});
+
+/* NODEMAILER CONTACT FORM */
+
+app.get("/contact", function (req, res){
+    res.render("contact");
+});
+
+app.post('/contact', function (req, res){
+
+    var newContact = {
+
+        firstName:  req.body.firstName,
+        lastName:   req.body.lastName,
+        email:      req.body.email,
+        text:       req.body.message,
+        subject:    "Contact Form Stilbruch.design"
+    };
+
+    var message = '<h1>New message from the contact form:</h1><h3 style="font-weight: normal">First Name: <p style="font-weight: bold; font-size: 1.3em; display: inline">' + newContact.firstName + '</p></h3><h3 style="font-weight: normal">Last Name: <p style="font-weight: bold; font-size: 1.3em; display: inline">' + newContact.lastName + '</p></h3><h3 style="font-weight: normal">E-Mail: <p style="font-weight: bold; font-size: 1.3em; display: inline">' + newContact.email + '</p></h3><h3 style="font-weight: normal">Message: <p style="font-weight: bold; font-size: 1.3em;">' + newContact.text + '</p></h3>';  
+
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+        host: process.env.NODEMAILER_HOST,
+        port: 465,
+        secure: true, // true for 465, false for other ports
+        auth: {
+            user: process.env.NODEMAILER_USER,
+            pass: process.env.NODEMAILER_PASS
+        }
+    });
+
+    // setup email data
+    let mailOptions = {
+        from: '"Contact Form Stilbruch.design" <info@stilbruch.design>', // sender address
+        to: 'info@stilbruch.design', // list of receivers
+        subject: newContact.subject, // Subject line
+        text: newContact.message, // plain text body
+        html: message // html body
+    };
+
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message sent: %s', info.messageId);
+    });
+
+    req.flash("success", "Vielen Dank, Ihre Nachricht wurde erfolgreich gesendet.");
+    res.redirect("/");
 });
 
 /* +++++++++++++++++++++++++++ APP LISTEN +++++++++++++++++++++++++++ */
--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,11 +1,12 @@
+$(window).resize(function(){
+  location.reload();
+  $('html').css({"height": $(document).height()});
+});
+
 $(document).ready(function(){
 /* ++++++++++++++++++++++++++++++++++++++++++++++  global  ++++++++++++++++++++++++++++++++++++++++++++++ */
 
 $('html').css({"height": $(document).height()});
-
-$(window).resize(function() {
-  $('html').css({"height": $(document).height()});
-});
 
 /* ++++++++++++++++++++++++++++++++++++++++++++++  navigation bar  ++++++++++++++++++++++++++++++++++++++++++++++ */
 $(".navMenu ").on("click", function(){
@@ -28,12 +29,32 @@ $(".navMenu ").on("click", function(){
   }); 
 });
 
+$(".navCancel ").on("click", function(){
+  
+  $(".navbar").css({
+
+    "display": "flex",
+    "flex-direction": "row",
+    "justify-content": "space-around",
+    "height": "50px" 
+  });
+  
+  $(".navItem:not(.navLogo):not(.navMenu)").css({
+    "display": "none"
+  });
+  
+  $(".navMenu").css({
+      "display": "block",
+      "min-width": "45vw"
+  }); 
+});
+
 // change background on scroll
 $(function(){
   $(window).scroll(function() {
       var scroll = $(window).scrollTop(); // how many pixels scrolled
-      var os = $('.mainLogo').offset().top; // pixels to the top of .mainLogo
-      var ht = $('.mainLogo').height(); // height of .mainLogo
+      var os = $('.mainLogo, .impressum h1').offset().top; // pixels to the top of .mainLogo
+      var ht = $('.mainLogo, impressum h1').height(); // height of .mainLogo
       var nav = $('.navbar').height(); // height of .navbar in pixels
       
       // if scrolled further than the top of .mainLogo minus height of .navbar, add class navBackground to .navbar, otherwise remove class navBackground
@@ -45,12 +66,88 @@ $(function(){
   });
 });
 
-/* ++++++++++++++++++++++++++++++++++++++++++++++  footer  ++++++++++++++++++++++++++++++++++++++++++++++ */
+var nav = $('.navbar').height() + 30;
 
-$(function() {
-  $('.footer-links-holder h3').click(function () {
-    $(this).parent().toggleClass('active');
-  });
+$("#e").on("click", function(x) {
+
+  x.preventDefault();
+  if (window.location.href != "http://localhost:3000/datenschutz" && window.location.href != "http://localhost:3000/impressum") {
+    $("body, html").animate({ 
+      scrollTop: $(".eGrid .gridHeader").offset().top - nav
+    }, 300);
+  } else {
+    window.location.href = "/";
+  }
+
+});
+
+$("#p").on("click", function(x) {
+
+  x.preventDefault();
+  if (window.location.href != "http://localhost:3000/datenschutz" && window.location.href != "http://localhost:3000/impressum") {
+    $("body, html").animate({ 
+      scrollTop: $(".pGrid .gridHeader").offset().top - nav
+    }, 300);
+  } else {
+    window.location.href = "/";
+  }
+
+});
+
+$("#s").on("click", function(x) {
+
+  x.preventDefault();
+  if (window.location.href != "http://localhost:3000/datenschutz" && window.location.href != "http://localhost:3000/impressum") {
+    $("body, html").animate({ 
+      scrollTop: $(".sGrid .gridHeader").offset().top - nav
+    }, 300);
+  } else {
+    window.location.href = "/";
+  }
+});
+
+
+$("#k, .fk").on("click", function(x) {
+
+  x.preventDefault();
+  if (window.location.href != "http://localhost:3000/datenschutz" && window.location.href != "http://localhost:3000/impressum") {
+    $("body, html").animate({ 
+      scrollTop: $(".kGrid .gridHeader").offset().top - nav
+    }, 300);
+  } else {
+    window.location.href = "/";
+  }
+});
+
+$('.cancel')
+  .on('click', function() {
+    $('.flash').hide();
+  })
+;
+
+/* ++++++++++++++++++++++++++++++++++++++++++++++  Cookie Banner CookieConsent.insites  ++++++++++++++++++++++++++++++++++++++++++++++ */
+
+window.addEventListener("load", function(){
+  window.cookieconsent.initialise({
+    "palette": {
+      "popup": {
+        "background": "rgba(24, 24, 24, 0.8)"
+      },
+    },
+    "type": "opt-out",
+    "content": {
+      href: "/datenschutz",
+      header: 'Cookies auf dieser Webseite',
+      message: 'Diese Webseite verwendet Cookies um sie für Sie optimal zu gestalten und fortlaufend verbessern zu können.',
+      dismiss: 'Habe ich vestanden.',
+      allow: 'Ok, stimme zu.',
+      deny: 'ablehnen',
+      link: 'Mehr über Cookies und Datenschutz erfahren.',
+      close: '&#x274c;',
+      policy: 'Cookie Infos',
+      target: '_blank',
+    }
+  })
 });
 
 /* ++++++++++++++++++++++++++++++++++++++++++++++  end of main.js  ++++++++++++++++++++++++++++++++++++++++++++++ */
