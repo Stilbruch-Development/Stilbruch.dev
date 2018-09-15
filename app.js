4f235c28e897d945365e87212afe8da25c5295be
Michael Hübner <m.huebner@email.com>
2018-09-15 14:22:29 +0200

    add gulpfile with autoprefix/minify css, uglify js, compress images and watch task, 15.09.2018

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 require('dotenv').config();
 app.set("view engine", "ejs");
-app.use("/public", express.static(__dirname + "/public"));
+app.use("/build/public", express.static(__dirname + "/build/public"));
 app.use("/views", express.static(__dirname + "/views"));
 app.use("/node_modules/cookieconsent", express.static(__dirname + "/node_modules/cookieconsent"));
 app.use(cookieParser());
