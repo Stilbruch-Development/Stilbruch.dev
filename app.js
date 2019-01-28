8396760e085f8681d646401477140dfa95201d46
Stilbruch-Design <info@stilbruch.design>
2019-01-28 05:42:25 +0100

    set up vscode

--- app.js
+++ app.js
@@ -1,123 +1,138 @@
-var cookieParser    = require('cookie-parser'),
-    nodemailer      = require('nodemailer'),
-    bodyParser      = require ("body-parser"),
-    express         = require ("express"),
-    flash           = require ("connect-flash"),
-    app             = express();
+const cookieParser = require('cookie-parser');
 
+const nodemailer = require('nodemailer');
 
+const bodyParser = require('body-parser');
+
+const express = require('express');
+
+const flash = require('connect-flash');
+
+const app = express();
 
 /* +++++++++++++++++++++++++++ APP CONFIGURATION +++++++++++++++++++++++++++ */
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 require('dotenv').config();
-app.set("view engine", "ejs");
-app.use("/build/public", express.static(__dirname + "/build/public"));
-app.use("/views", express.static(__dirname + "/views"));
-app.use("/node_modules/cookieconsent", express.static(__dirname + "/node_modules/cookieconsent"));
+
+app.set('view engine', 'ejs');
+app.use('/build/public', express.static(`${__dirname}/build/public`));
+app.use('/views', express.static(`${__dirname}/views`));
+app.use('/node_modules/cookieconsent', express.static(`${__dirname}/node_modules/cookieconsent`));
 app.use(cookieParser());
-app.use(require("express-session")({
+app.use(
+  require('express-session')({
     secret: process.env.SESSIONSECRET,
-    cookie: {_expires : 30000000},
+    cookie: { _expires: 30000000 },
     resave: false,
-    saveUninitialized: false
-}));
+    saveUninitialized: false,
+  })
+);
+
 app.use(flash());
 
 /* +++++++++++++++++++++++++ GLOBAL ELEMENTS CONFIGURATION +++++++++++++++++++++++++ */
-app.use(function(req, res, next){
-    res.locals.error = req.flash("error");
-    res.locals.success = req.flash("success");
-    next();
- });
+app.use((req, res, next) => {
+  res.locals.error = req.flash('error');
+  res.locals.success = req.flash('success');
+  next();
+});
 
 /* ++++++++++++++++++++++++++++++ APP ROUTING ++++++++++++++++++++++++++++++ */
 /* ROOT ROUTE/ LANDING */
-app.get("/", function(req, res){
-    res.render("landing");
+app.get('/', (req, res) => {
+  res.render('landing');
 });
 
 /* IMPRESSUM ROUTE */
-app.get("/impressum", function(req, res){
-    res.render("impressum");
+app.get('/impressum', (req, res) => {
+  res.render('impressum');
 });
 
 /* DATENSCHUTZ ROUTE */
-app.get("/datenschutz", function(req, res){
-    res.render("datenschutz");
+app.get('/datenschutz', (req, res) => {
+  res.render('datenschutz');
 });
 
 /* NODEMAILER CONTACT FORM */
 
-app.get("/kontakt", function (req, res){
-    res.render("contact");
+app.get('/kontakt', (req, res) => {
+  res.render('contact');
 });
 
-app.post('/kontakt', function (req, res){
+app.post('/kontakt', (req, res) => {
+  const newContact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    text: req.body.message,
+    subject: 'Kontakt von Stilbruch.design',
+  };
 
-    var newContact = {
+  const botContact = {
+    firstName: req.body.firstName1,
+    lastName: req.body.lastName1,
+    email: req.body.email1,
+    text: req.body.message1,
+  };
 
-        firstName:  req.body.firstName,
-        lastName:   req.body.lastName,
-        email:      req.body.email,
-        text:       req.body.message,
-        subject:    "Kontakt von Stilbruch.design"
-    };
+  const botArr = Object.values(botContact);
 
-    const botContact = {
-        firstName: req.body.firstName1,
-        lastName: req.body.lastName1,
-        email: req.body.email1,
-        text: req.body.message1,
-      };
-    
-      const botArr = Object.values(botContact);
-    
-      function notEmpty(value) {
-        return value !== '';
-      }
+  function notEmpty(value) {
+    return value !== '';
+  }
 
-    var message = '<h1>New message from the contact form:</h1><h3 style="font-weight: normal">First Name: <p style="font-weight: bold; font-size: 1.3em; display: inline">' + newContact.firstName + '</p></h3><h3 style="font-weight: normal">Last Name: <p style="font-weight: bold; font-size: 1.3em; display: inline">' + newContact.lastName + '</p></h3><h3 style="font-weight: normal">E-Mail: <p style="font-weight: bold; font-size: 1.3em; display: inline">' + newContact.email + '</p></h3><h3 style="font-weight: normal">Message: <p style="font-weight: bold; font-size: 1.3em;">' + newContact.text + '</p></h3>';  
+  const message = `<h1>New message from the contact form:</h1><h3 style="font-weight: normal">First Name: <p style="font-weight: bold; font-size: 1.3em; display: inline">${
+    newContact.firstName
+  }</p></h3><h3 style="font-weight: normal">Last Name: <p style="font-weight: bold; font-size: 1.3em; display: inline">${
+    newContact.lastName
+  }</p></h3><h3 style="font-weight: normal">E-Mail: <p style="font-weight: bold; font-size: 1.3em; display: inline">${
+    newContact.email
+  }</p></h3><h3 style="font-weight: normal">Message: <p style="font-weight: bold; font-size: 1.3em;">${
+    newContact.text
+  }</p></h3>`;
 
-    // create reusable transporter object using the default SMTP transport
-    let transporter = nodemailer.createTransport({
-        host: process.env.NODEMAILER_HOST,
-        port: 465,
-        secure: true, // true for 465, false for other ports
-        auth: {
-            user: process.env.NODEMAILER_USER,
-            pass: process.env.NODEMAILER_PASS
-        }
-    });
+  // create reusable transporter object using the default SMTP transport
+  const transporter = nodemailer.createTransport({
+    host: process.env.NODEMAILER_HOST,
+    port: 465,
+    secure: true, // true for 465, false for other ports
+    auth: {
+      user: process.env.NODEMAILER_USER,
+      pass: process.env.NODEMAILER_PASS,
+    },
+  });
 
-    // setup email data
-    let mailOptions = {
-        from: '"Contact Form Stilbruch.design" <info@stilbruch.design>', // sender address
-        to: 'info@stilbruch.design', // list of receivers
-        subject: newContact.subject, // Subject line
-        text: newContact.message, // plain text body
-        html: message // html body
-    };
+  // setup email data
+  const mailOptions = {
+    from: '"Contact Form Stilbruch.design" <info@stilbruch.design>', // sender address
+    to: 'info@stilbruch.design', // list of receivers
+    subject: newContact.subject, // Subject line
+    text: newContact.message, // plain text body
+    html: message, // html body
+  };
 
-    if (botArr.some(notEmpty) === true) {
-        // req.flash('error', 'There was an error with your form submission. Please try again.');
-        res.redirect('/');
-        return undefined;
-      } else {
-        // send mail with defined transport object
-        transporter.sendMail(mailOptions, (error, info) => {
-            if (error) {
-                return console.log(error);
-            }
-            console.log('Message sent: %s', info.messageId);
-        });
-
-        req.flash("success", "Vielen Dank, Ihre Nachricht wurde erfolgreich gesendet. Ich melde mich so schnell wie möglich.");
-        res.redirect("/");
+  if (botArr.some(notEmpty) === true) {
+    // req.flash('error', 'There was an error with your form submission. Please try again.');
+    res.redirect('/');
+    return undefined;
+  }
+  // send mail with defined transport object
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
     }
+    console.log('Message sent: %s', info.messageId);
+    return undefined;
+  });
+
+  req.flash(
+    'success',
+    'Vielen Dank, Ihre Nachricht wurde erfolgreich gesendet. Ich melde mich so schnell wie möglich.'
+  );
+  res.redirect('/');
+  return undefined;
 });
 
 /* +++++++++++++++++++++++++++ APP LISTEN +++++++++++++++++++++++++++ */
-app.listen(process.env.PORT || 3000, process.env.ID, function(){
-	console.log ("Server started on port 3000 ...");
-});
+app.listen(process.env.PORT || 3000, process.env.ID, () => console.log('Server started on port 3000 ...'));
--- gulpfile.js
+++ gulpfile.js
@@ -1,60 +1,65 @@
-var gulp = require('gulp'),
+const gulp = require('gulp'),
   uglify = require('gulp-uglify-es').default,
   rename = require('gulp-rename'),
   autoprefixer = require('autoprefixer'),
-//   minifyejs = require('gulp-minify-ejs'),
+  //   minifyejs = require('gulp-minify-ejs'),
   postcss = require('gulp-postcss'),
   imagemin = require('gulp-imagemin'),
   cleanCss = require('gulp-clean-css'),
   babel = require('gulp-babel');
 //   browserify = require('gulp-run-command').default;
 
-
 // minify javascript
-gulp.task ('scripts', function(){
-
-    gulp.src('public/javascript/*.js')
-        .pipe(babel())
-        .pipe(uglify())
-        .pipe(rename(function (path) {
-            path.basename += ".min";
-            path.extname = ".js";
-          }))
-        .pipe(gulp.dest('build/public/javascript'));
-
+gulp.task('scripts', function() {
+  gulp
+    .src('public/javascript/*.js')
+    .pipe(babel())
+    .pipe(uglify())
+    .pipe(
+      rename(function(path) {
+        path.basename += '.min';
+        path.extname = '.js';
+      })
+    )
+    .pipe(gulp.dest('build/public/javascript'));
 });
 
 // autoprefixe css
-gulp.task('styles', function () {
-
-    return gulp.src('public/stylesheets/*.css')
-        .pipe(postcss([ autoprefixer({browsers: ['IE 11']})]))
-        .pipe(cleanCss())
-        .pipe(rename(function (path) {
-            path.basename += ".prefixed.min";
-            path.extname = ".css";
-          }))
-        // .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('build/public/stylesheets'));
+gulp.task('styles', function() {
+  return (
+    gulp
+      .src('public/stylesheets/*.css')
+      .pipe(postcss([autoprefixer({ browsers: ['IE 11'] })]))
+      .pipe(cleanCss())
+      .pipe(
+        rename(function(path) {
+          path.basename += '.prefixed.min';
+          path.extname = '.css';
+        })
+      )
+      // .pipe(sourcemaps.write('.'))
+      .pipe(gulp.dest('build/public/stylesheets'))
+  );
 });
 
 // compress images
-gulp.task('compress', function () {
-
-    return gulp.src('public/images/*')
-        .pipe(imagemin())
-        .pipe(rename(function (path) {
-            path.basename += ".compressed";
-          }))
-        .pipe(gulp.dest('build/public/images'));
+gulp.task('compress', function() {
+  return gulp
+    .src('public/images/*')
+    .pipe(imagemin())
+    .pipe(
+      rename(function(path) {
+        path.basename += '.compressed';
+      })
+    )
+    .pipe(gulp.dest('build/public/images'));
 });
-
 
 // watch for changes
-gulp.task('watch', function () {
-        gulp.watch('public/javascript/*.js', ['scripts']);
-        gulp.watch('public/stylesheets/*.css', ['styles']);
-        gulp.watch('public/images/*', ['compress']);        
+gulp.task('watch', function() {
+  gulp.watch('public/javascript/*.js', ['scripts']);
+  gulp.watch('public/stylesheets/*.css', ['styles']);
+  gulp.watch('public/images/*', ['compress']);
 });
 
-gulp.task ('default', ['scripts', 'styles', 'compress', 'watch']);
+gulp.task('default', ['scripts', 'styles', 'compress', 'watch']);
